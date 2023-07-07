be5752a9ba666d18054ec2d5c728083c3bc2a895
Matthew Phan <mttphan261@gmail.com>
2023-07-07 17:04:54 -0500

    authentication functional

--- server/app.py
+++ server/app.py
@@ -1,14 +1,19 @@
 from flask_migrate import Migrate
-from models import Character
+from models import Character, Move, User, UserCharacter
 from flask import Flask, request, session, make_response, jsonify, redirect
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from config import app, db, api, Resource
 import ipdb
+import traceback
 
 
 migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
 
 class Characters(Resource):
     def get(self):
@@ -29,6 +34,101 @@ class CharacterByName(Resource):
             return {'error': 'fighter not found'}, 404
 
 api.add_resource(CharacterByName, '/characters/<string:name>')
+
+#**********AUTHENTICATION ROUTES**********
+class Users(Resource):
+    @login_required
+    def get(self):
+        try:
+            user = current_user
+            if user:
+                return {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email
+                }, 200
+            else:
+                return {"error": "User not found"}, 404
+        except:
+            return {"error": "An error occurred while fetching the user"}, 500
+    
+    @login_required
+    def delete(self):
+        try:
+            user = current_user
+            if user:
+                db.session.delete(user)
+                db.session.commit()
+                return {}, 204
+            else:
+                return {"error": "User not found"}, 404
+        except:
+            return {"error":"An error occurred while deleting the user"}, 500
+
+
+api.add_resource(Users, '/users')
+
+class Signup(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            new_user = User(
+                username=data['username'],
+                email=data['email'],
+            )
+            new_user.password_hash = data['password']
+            db.session.add(new_user)
+            db.session.commit()
+
+            # session['user_id'] = new_user.id
+            login_user(new_user, remember=True)
+
+            return new_user.to_dict(), 201
+        
+        except Exception as e:
+            traceback.print_exc()
+            return {"error": "Signup failed.", "message": str(e)}, 500
+
+api.add_resource(Signup, '/signup')
+
+
+class Login(Resource):
+
+    def post(self):
+        try:
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+
+            user = User.query.filter(User.username == username).first()
+
+            if user:
+                if user.authenticate(password):
+                    login_user(user, remember=True)
+                    return {'message': 'Login successful'}, 200
+            return {'error': '401 Unauthorized'}, 401
+        
+        except Exception as e:
+            traceback.print_exc()
+            return {"error": "Signup failed.", "message": str(e)}, 500        
+
+
+api.add_resource(Login, '/login')
+
+class CheckSession(Resource):
+    def get(self):
+        if current_user.is_authenticated:
+            user = current_user.to_dict()
+            return user, 200
+        return {"error": "unauthorized"}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
+@app.route("/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return f'You have logged out. Goodbye'
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,6 +7,8 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from collections import OrderedDict
 from flask_login import UserMixin, LoginManager
+import re
+from datetime import datetime
 
 from config import db, bcrypt
 
@@ -20,8 +22,11 @@ class Character(db.Model, SerializerMixin):
     head_img = db.Column(db.String)
     main_img = db.Column(db.String)
 
-    moves = db.relationship('Move', back_populates='character')
+    #relationships
+    moves = db.relationship('Move', back_populates='character')    #relationships
+    user_characters = db.relationship('UserCharacter', back_populates='character')
 
+    #serialization
     serialize_rules = ('-moves.character',)
 
     def __repr__(self):
@@ -35,11 +40,93 @@ class Move(db.Model, SerializerMixin):
     command = db.Column(db.String)
 
     character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+
     #relationships
     character = db.relationship('Character', back_populates='moves')
 
+    #serialization
     serialize_rules = ('-character.moves',)
 
     def __repr__(self):
         return f'Move: {self.id}, Character: {self.character_id}'
 
+class User(db.Model, SerializerMixin, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #VALIDATIONS
+    @validates('email')
+    def validate_email(self, key, email):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise ValueError('Invalid email format')
+        return email
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username and len(username) < 1:
+            raise ValueError('Invalid username')
+        return username
+
+    #PASSWORD HASHING
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    #relationships
+    user_characters = db.relationship('UserCharacter', back_populates='user')
+
+    #serialization
+    serialize_rules = ('-user.user_characters',)
+
+    def __repr__(self):
+        return f'''
+        ID: {self.id},
+        Username: {self.username},
+        Email: {self.email}
+        UserCharacters: {self.user_character}
+        '''
+    
+class UserCharacter(db.Model, SerializerMixin):
+    __tablename__ = 'user_characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    is_main = db.Column(db.Boolean)
+    is_alt = db.Column(db.Boolean)
+    #SAVED VIDEOS WILL GO HERE
+
+    #FKs
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    
+    #relationships
+    user = db.relationship('User', back_populates='user_characters')
+    character = db.relationship('Character', back_populates='user_characters')
+
+    #serialization
+    serialize_rules = ('-user.user_characters',)
+
+    def __repr__(self):
+        return f'''
+        ID: {self.id}
+        Character: {self.character.name}
+        User: {self.user.username},
+        '''
+
+
+    
